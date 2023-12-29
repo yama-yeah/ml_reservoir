d4b87dbac0c6d10284f3fd3c2176dbfb56c13d10
yama-yeah <b1021091@fun.ac.jp>
2023-12-29 15:59:26 +0900

    add: export matrix utils

--- lib/ml_reservoir.dart
+++ lib/ml_reservoir.dart
@@ -5,6 +5,7 @@ library;
 
 export 'src/regressor/regressor.dart';
 export 'src/reservoir/reservoir.dart';
+export 'src/utils/matrix_utils.dart';
 
 
 // TODO: Export any libraries intended for clients of this package.
